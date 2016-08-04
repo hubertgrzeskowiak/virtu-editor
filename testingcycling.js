95630bffb35fad55dfa3447ecd420a518f5f1757
Hubert Grzeskowiak <hubertgrzeskowiak@users.noreply.github.com>
2016-08-04 14:43:09 +0200

    removing old unnecessary files

--- testingcycling.js
+++
@@ -1,9 +0,0 @@
-rootScope = angular.element($0).scope()
-
-node = rootScope;
-while (node != null) {
-    if (node.$ctrl != null) {
-        console.log(node.$ctrl.constructor.name)
-    }
-    node = node.$$childHead;
-}
