bee756036c642f8461da7ec5cc58d1cb77896162
Hubert Grzeskowiak <hubertgrzeskowiak@users.noreply.github.com>
2016-08-06 15:47:14 +0200

    inquiry model

--- app/core/inquiry/inquiry.js
+++ app/core/inquiry/inquiry.js
@@ -10,3 +10,13 @@ function InquiryController() {
         this.isFocused = false;
     }
 }
+
+function Inquiry(target, choices) {
+    this.target = defaultValue(target, "");
+    this.choices = defaultValue(choices, []);
+}
+
+function Choice(result, text) {
+    this.result = defaultValue(result, "");
+    this.text = defaultValue(text, "");
+}
