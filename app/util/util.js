5f1c2fcc5dff9aa3f2981cb99e5301f1f99bd75c
Hubert Grzeskowiak <hubertgrzeskowiak@users.noreply.github.com>
2016-08-07 22:30:00 +0200

    items in conversation now properly rendered with dynamic components

--- app/util/util.js
+++ app/util/util.js
@@ -32,26 +32,23 @@ angular.module("util.autoGrow", [])
             };
         })
 
-    .directive('component', ['$compile',
-        function ($compile) {
-            return {
-                restrict: 'AEM',
-                scope: {
-                    name: '<',
-                    args: '<'
-                },
-                controller: function ($scope, $element) {
-                    var args = $scope.args;
-                    var argsStr = ""
-                    if (typeof args !== 'undefined' && args.constructor === Object) {
-                        for (arg in args) {
-                            argsStr += ' ' + arg + '="args.' + arg + '"';
-                        }
-                    }
-                    var elem = "<" + $scope.name + argsStr + "></" + $scope.name + ">";
-                    var component = $compile(elem)($scope)[0];
+    .directive("component", ["$compile", '$parse', function ($compile, $parse) {
+        return {
+            controller: function ($scope, $element) {
+                var name = $parse($element.attr("name"))($scope);
+                var args = $parse($element.attr("args"))($scope);
+                var replace = $parse($element.attr("replace"))($scope);
+                var argsStr = " ";
+                angular.forEach(args, function (value, key, obj) {
+                    argsStr += key + '="' + value + '" ';
+                });
+                var elem = "<" + name + argsStr + "></" + name + ">";
+                var component = $compile(elem)($scope)[0];
+                if (replace) {
+                    $element.replaceWith(component);
+                } else {
                     $element.append(component);
                 }
             }
         }
-    ])
+    }])
