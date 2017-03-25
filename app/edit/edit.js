782752ea76a0f1dd66e30b7e8a64bdbd7d52d8d2
Hubert Grzeskowiak <hubertgrzeskowiak@users.noreply.github.com>
2017-03-25 12:15:03 +1100

    mostly cosmetic code cleanup

--- app/edit/edit.js
+++ app/edit/edit.js
@@ -1,12 +1,12 @@
-angular.module('myApp.edit', ['ngRoute', 'navigationutil', 'myApp.message', 'myApp.conversation', 'jutaz.ngSweetAlertAsPromised'])
+angular.module("myApp.edit", ["ngRoute", "navigationutil", "myApp.message", "myApp.conversation", "jutaz.ngSweetAlertAsPromised"])
 
-    .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/edit', {
-            templateUrl: 'edit/edit.html',
+    .config(["$routeProvider", function ($routeProvider) {
+        $routeProvider.when("/edit", {
+            templateUrl: "edit/edit.html"
         });
     }])
 
-    .controller('EditController', ['$rootScope', 'SweetAlert', 'conversations', EditController])
+    .controller("EditController", ["$rootScope", "SweetAlert", "conversations", EditController])
 
     .config(function (conversationsProvider) {
         var startingConversations = [
@@ -22,47 +22,50 @@ angular.module('myApp.edit', ['ngRoute', 'navigationutil', 'myApp.message', 'myA
             ])
         ];
         conversationsProvider.setInitialData(startingConversations);
-    })
+    });
 
 
 function EditController($rootScope, SweetAlert, convService) {
     var $ctrl = this;
     this.$onInit = function () {
         this.conversations = convService.all;
-    }
+    };
 
     this.addConversation = function (character_id) {
         var c = convService.add("new character");
         c.items.push(new RandomMessage());
         return c;
-    }
+    };
+
     this.askRemoveConversation = function (conversation) {
         var cancelMessages = ["Gosh, no!", "Wait!", "Aaah misclick!", "Stay!", "Actually..."];
         var confirmMessages = ["Be gone!", "Farewell", "I said: DELETE!", "No mercy!", "See ya!"];
         var randomCancel = function () {
             return cancelMessages[Math.floor(Math.random() * cancelMessages.length)];
-        }
+        };
         var randomConfirm = function () {
             return confirmMessages[Math.floor(Math.random() * confirmMessages.length)];
-        }
+        };
 
         SweetAlert.swal({
-            title: "<div class='conversation-header'>" +
-                "<div class='conversation-image'></div>" +
+            title: "<div class=\"conversation-header\">" +
+                "<div class=\"conversation-image\"></div>" +
                 "</div>" +
                 "Are you sure?",
             text: "Removing the whole conversation is permanent!",
             showCancelButton: true,
             cancelButtonText: randomCancel(),
             confirmButtonText: randomConfirm(),
+            reverseButtons: true,
             confirmButtonColor: "#D9534F"
         }).then(function () {
             $ctrl.removeConversation(conversation);
-        }, function() {
+        }, function () {
             // aborted
-        })
-    }
+        });
+    };
+
     this.removeConversation = function (conversation) {
         convService.remove(conversation.key);
-    }
-}
+    };
+};
